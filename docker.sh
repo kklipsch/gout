d67e0b582f07d4ed8d6cce6e926c1339fb87a596
kklipsch <kklipsch@example.com>
2017-12-27 18:21:22 +0000

    just flailing at this point

--- docker.sh
+++ docker.sh
@@ -4,6 +4,6 @@ set -eu
 
 TAG=$1
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_API_KEY"
+docker login -u $DOCKER_USERNAME -p $DOCKER_API_KEY
 docker build -t $DOCKER_USERNAME/gout:$TAG .
 docker push $DOCKER_USERNAME/gout:$TAG
