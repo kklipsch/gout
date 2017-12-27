4c47737b89694a491f76932fd1a35e36f69cbf95
kklipsch <kklipsch@example.com>
2017-12-27 19:27:42 +0000

    remove docker stuff cause i can't figure out travis

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM scratch
-
-ADD gout /
-CMD ["/gout", "hello world"] 
--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-TAG=$1
-
-docker login -u $DOCKER_USERNAME -p $DOCKER_API_KEY
-docker build -t $DOCKER_USERNAME/gout:$TAG .
-docker push $DOCKER_USERNAME/gout:$TAG
